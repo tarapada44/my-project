e4f7a46a391ad2d644cc5682fcff6b46156da517
Tarapada Panda <pinkupik77@gmail.com>
2021-02-05 15:24:38 +0530

    Initial commit

--- Projects/Projects.ConsoleApp1/Properties/AssemblyInfo.cs
+++ Projects/Projects.ConsoleApp1/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c8a46c59-d6f7-4755-9d15-abafe2ba81b1")]
+[assembly: Guid("87430ea3-ccee-4306-b00c-335fba3674f2")]
 
 // Version information for an assembly consists of the following four values:
 //
